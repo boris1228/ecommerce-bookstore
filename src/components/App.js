397a00f2b539278536f9d11b846eacebf853a342
yf-chang <ivan1995726@hotmail.com>
2021-07-02 12:08:31 -0400

    footer update

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
+//import GeneralRoute from './Router/GeneralRoute';
+//import Header from './Header';
+import Footer from './Footer';
+//import Home from './Home';
+import '../App.css';
+
+function App() {
+
+
+  return (
+    // <BrowserRouter>
+    //   <Header />   
+    //   <Switch>        
+    //     {/* <Route path="/product">
+    //       <GeneralRoute/>
+    //     </Route> */}
+    //     <Route path="/">    
+    //       <Home />                  
+    //     </Route>        
+    //   </Switch>
+    //   <Footer/>    
+    // </BrowserRouter>
+    <Footer/>
+  );
+}
+
+
+export default App;
