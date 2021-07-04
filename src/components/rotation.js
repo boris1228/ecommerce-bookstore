f7df34a4634786c14ff7f09ad095e821e3cc52a2
Yichen Zhan <boris1228@gmail.com>
2021-07-04 15:58:47 -0400

    Add files via upload

---
+++ src/components/rotation.js
@@ -0,0 +1,23 @@
+import {Carousel, Container} from 'react-bootstrap';
+
+export default function Book(){
+    const bookImgs = ['book1.png','book2.png']
+
+    return(
+        <div className="mt-57">
+        <Container className="pg-max-width">
+            <Carousel>
+                {bookImgs.map(item=>(
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={`./img/book/${item}`}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                ))};
+            </Carousel>
+        </Container>
+        </div>
+    )
+}
