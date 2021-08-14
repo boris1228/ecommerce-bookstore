135985359c0e24f2fac283b784dedb48f81a16c3
proDeveloper-CA <85241627+prodeveloper-ca@users.noreply.github.com>
2021-08-14 14:45:05 -0600

    footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react'
-import { BrowserRouter, Route, Switch, useParams } from 'react-router-dom';
+import {useState, useEffect} from 'react'
+import {BrowserRouter, Route, Switch, useParams} from 'react-router-dom';
 import GeneralRoute from './Router/GeneralRoute';
 import AdminRouter from './Router/AdminRouter';
 import UserRouter from './Router/UserRouter';
@@ -8,65 +8,77 @@ import Footer from './Component/Footer';
 import Home from './Component/Home';
 import OrderList from './pages/OrderList';
 import OrderDetails from './pages/OrderDetails';
+import About from "./Component/about";
+import Policy from "./Component/policy";
+import ContactUs from "./Component/contactUs";
 
-import { auth } from './Component/firebase';
-import { useStateValue } from './Component/StateProvider'
+import {auth} from './Component/firebase';
+import {useStateValue} from './Component/StateProvider'
 
 //
 
 function App() {
-  const [allMovies, setMovies] = useState([]);
-  const [{ }, dispatch] = useStateValue();
-
-  useEffect(() => {
-    auth.onAuthStateChanged(authUser => {
-      console.log("User is: ", authUser);
-      if (authUser) {
-        dispatch({
-          type: 'SET_USER',
-          user: authUser
+    const [allMovies, setMovies] = useState([]);
+    const [{}, dispatch] = useStateValue();
+    
+    useEffect(() => {
+        auth.onAuthStateChanged(authUser => {
+            console.log("User is: ", authUser);
+            if (authUser) {
+                dispatch({
+                    type: 'SET_USER',
+                    user: authUser
+                })
+            } else {
+                dispatch({
+                    type: 'SET_USER',
+                    user: null
+                })
+            }
         })
-      } else {
-        dispatch({
-          type: 'SET_USER',
-          user: null
-        })
-      }
-    })
-  }, [])
-
-
-  useEffect(() => {
-    fetch("https://afternoon-caverns-41327.herokuapp.com/books")
-      .then((res) => res.json())
-      .then((movies) => {
-        setMovies(movies);
-      });
-  }, []);
-
-  return (
-    <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route exact path="/user">
-          <UserRouter />
-        </Route>
-        <Route exact path="/product">
-          <GeneralRoute />
-        </Route>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/orderlist">
-          <OrderList />
-        </Route>
-        <Route exact path="/orderdetails/:id">
-          <OrderDetails />
-        </Route>
-      </Switch>
-      <Footer />
-    </BrowserRouter>
-  );
+    }, [])
+    
+    
+    useEffect(() => {
+        fetch("https://afternoon-caverns-41327.herokuapp.com/books")
+            .then((res) => res.json())
+            .then((movies) => {
+                setMovies(movies);
+            });
+    }, []);
+    
+    return (
+        <BrowserRouter>
+            <Header/>
+            <Switch>
+                <Route exact path="/user">
+                    <UserRouter/>
+                </Route>
+                <Route exact path="/product">
+                    <GeneralRoute/>
+                </Route>
+                <Route exact path="/contactus">
+                    <ContactUs/>
+                </Route>
+                <Route exact path="/about">
+                    <About/>
+                </Route>
+                <Route exact path="/Policy">
+                    <Policy/>
+                </Route>
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+                <Route exact path="/orderlist">
+                    <OrderList/>
+                </Route>
+                <Route exact path="/orderdetails/:id">
+                    <OrderDetails/>
+                </Route>
+            </Switch>
+            <Footer/>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,56 +1,63 @@
 import React from 'react';
-import { SocialIcon } from 'react-social-icons';
+import {SocialIcon} from 'react-social-icons';
 import '../css/Footer.css';
-  
+
 import {Row, Col} from 'react-bootstrap';
+import {Link} from "react-router-dom";
 
 const Footer = () => {
-    const socialIconStyle = {  height: 40, width: 40};
+    const socialIconStyle = {height: 40, width: 40};
     return (
-        <div className = "pg-max-width">
-            <div className = "ml-5">
-
-                <div className = "mt-3">
+        <div className="pg-max-width">
+            <div className="ml-5">
+                
+                <div className="mt-3">
                     <h3>Logo</h3>
                 </div>
-
-                <Row className = "mt-4">
+                
+                <Row className="mt-4">
                     <Col><h3>Follow Us</h3>
-                    <ul className="foot-col-top-row">
-                        <li><SocialIcon url="https://facebook.com" style={socialIconStyle} fgColor="white"/> Facebook</li>
-                        <li><SocialIcon url="https://instagram.com" style={socialIconStyle} fgColor="white"/> Instgram</li>
-                        <li><SocialIcon url="https://twitter.com" style={socialIconStyle} fgColor="white"/> Twitter</li>
-                    </ul>
+                        <ul className="foot-col-top-row">
+                            <li><SocialIcon url="https://facebook.com" style={socialIconStyle}
+                                            fgColor="white"/> Facebook
+                            </li>
+                            <li><SocialIcon url="https://instagram.com" style={socialIconStyle}
+                                            fgColor="white"/> Instgram
+                            </li>
+                            <li><SocialIcon url="https://twitter.com" style={socialIconStyle} fgColor="white"/> Twitter
+                            </li>
+                        </ul>
                     </Col>
                     
-  
-                <Col>
-                <h3>Help</h3>
-                    <ul className="foot-col-top-row">
-                        <li>About</li>
-                        <li>Contact Us</li>
-                        <li>Policy</li>
-                    </ul>
-                </Col>
-     
-                <Col>
-                <h3>My Account</h3>
-                    <ul className="foot-col-top-row">
-                        <li>Log in</li>
-                        <li>Profile</li>
-                        <li>Wishlist</li>
-                        <li>Order History</li>
-                    </ul>
-                </Col>
-  
-            </Row>
-            <hr />
-            <div className = "foot-last-info">
-                <p className="foot-separator">
-                    &copy;{new Date().getFullYear()} MyFlix Movie Database | All rights reserved 
-                </p>
+                    
+                    <Col>
+                        <h3>Help</h3>
+                        <ul className="foot-col-top-row">
+                            <li><Link to="/about">About</Link></li>
+                            <li><Link to="/contactus">Contact US</Link></li>
+                            <li><Link to="/Policy">Policy </Link></li>
+                        
+                        </ul>
+                    </Col>
+                    
+                    <Col>
+                        <h3>My Account</h3>
+                        <ul className="foot-col-top-row">
+                            <li>Log in</li>
+                            <li>Profile</li>
+                            <li>Wishlist</li>
+                            <li>Order History</li>
+                        </ul>
+                    </Col>
+                
+                </Row>
+                <hr/>
+                <div className="foot-last-info">
+                    <p className="foot-separator">
+                        &copy;{new Date().getFullYear()} MyFlix Movie Database | All rights reserved
+                    </p>
+                </div>
             </div>
-        </div>
         </div>
     )
 }
